ec4abbfe041a0a19e144a03cad1c4f2474ecea24
juanorjuela <juancamiloorjuela@gmail.com>
2025-05-15 18:32:47 -0500

    fs

--- script.js
+++ script.js
@@ -1,62 +1,78 @@
 // Card Decks
 let playerDeck = [
 
-    ...Array(4).fill("🍕<br> <br>Order Pizza"),
-    ...Array(4).fill("🍻<br> <br>Order Drinks"),
-    ...Array(4).fill("🔊<br> <br>Hire a new DJ Station"),
-    ...Array(2).fill("🏃<br> <br>Rush:<br> <br>You got extra Dice. <br> <br>This turn you move with 3 dice"),
-    ...Array(2).fill("🏇<br> <br>Freedom: <br> <br>You got an extra MOVE this round"),
-    ...Array(4).fill("🛁<br> <br>Add access to a new 🚾 "),
-    ...Array(4).fill("🚪<br> <br>Come In:<br> <br>Everyone in the entrance moves inside"),
-    ...Array(3).fill("🧽<br> <br>Clean 🚾<br> <br>Everyone in the 🚾 must go elsewhere"),
-    ...Array(8).fill("🥸<br> <br>Invite Random Guest from a genre of your choosing"),
-    ...Array(3).fill("🎵<br> <br>Play Music:<br> <br>Rock<br> <br>3 Rockers arrive to the party and someone who hates rock music leaves"),
-    ...Array(3).fill("🎵<br> <br>Play Music:<br> <br>Pop<br> <br>3 Poppers arrive to the party and someone who hates pop music leaves"),
-    ...Array(3).fill("🎵<br> <br>Play Music:<br> <br>Latin<br> <br>3 Latins arrive to the party and someone who hates rock music leaves"),
-    ...Array(3).fill("🎵<br> <br>Play Music:<br> <br>Rap<br> <br>3 Rappers arrive to the party and someone who hates rap music leaves"),
-    ...Array(3).fill("🎵<br> <br>Play Music:<br> <br>Techno<br> <br>3 Ravers arrive to the party and someone who hates techno music leaves"),
-    ...Array(3).fill("🎵<br> <br>Play Music:<br> <br>Disco<br> <br>3 Discoheads arrive to the party and someone who hates disco music leaves"),
-    ...Array(2).fill("🎉<br> <br>The DROP:<br> <br>Your party is so lit that you are able to cancel the effect of one FCKUP"),
+    ...Array(3).fill("<div><h4>🍕<br> <br>Order Pizza</h4></div>"),
+    ...Array(3).fill("<div><h4>🍻<br> <br>Order Drinks</h4></div>"),
+    ...Array(2).fill("<div><h4>🔊<br> <br>Add a new dancefloor</h4></div>"),
+    ...Array(2).fill("<div><h4>🚾<br> <br>Add a new Toilet </h4></div>"),
+
+    ...Array(1).fill("<div><h4>🏃 Rush: </h4><br> <p>This turn you move with 3 dice<p/></div>"),
+
+    ...Array(2).fill("<div><h4>🚪<br> Come In: <br>Everyone in the entrance moves inside</h4></div>"),
+    ...Array(2).fill("<div><h4>🧽<br> Clean 🚾: <br>Everyone in the toilet must go elsewhere</h4></div>"),
+    ...Array(4).fill("<div><h4>🥸<br> <br>Invite Random Guest from a genre of your choosing</h4></div>"),
+
+    ...Array(3).fill("<div> <h4>🎵 Play Music:<br> <br>Rock<br></h4><p>3 Rockers arrive to the party and someone who hates rock music leaves</p></div>"),
+    ...Array(3).fill("<div> <h4>🎵 Play Music:<br> <br>Pop<br></h4><p>3 Poppers arrive to the party and someone who hates pop music leaves</p></div>"),
+    ...Array(3).fill("<div> <h4>🎵 Play Music:<br> <br>Latin<br></h4><p>3 Latinos arrive to the party and someone who hates latin music leaves</p></div>"),
+    ...Array(3).fill("<div> <h4>🎵 Play Music:<br> <br>Rap<br></h4><p>3 Rappers arrive to the party and someone who hates rap music leaves</p></div>"),
+    ...Array(3).fill("<div> <h4>🎵 Play Music:<br> <br>Techno<br></h4><p>3 Ravers arrive to the party and someone who hates techno music leaves</p></div>"),
+    ...Array(3).fill("<div> <h4>🎵 Play Music:<br> <br>Disco<br></h4><p>3 Discoheads arrive to the party and someone who hates disco music leaves</p></div>"),
+
+    ...Array(1).fill("<div><h4>🎉 The DROP:</h4><br><p>Your party is so lit that you are able to cancel the effect of one FCKUP</p></div>"),
 ];
 
 const fuckupsDeck = [
-    ...Array(6).fill("🎶 Change Music: Go to the dancefloor room and play any music card from your hand. If the music doesn't change now, 2 guests will leave the party."),
-    ...Array(1).fill("🐈‍⬛ Unlucky: Draw 2 more FCKUP cards."),
-    ...Array(1).fill("💤 Lazy Bastard: Discard 3 action cards from your hand. You can only play with 3 cards for the next round."),
-    ...Array(1).fill("🥴 Too Much to Drink: You can only play 1 action per round for the next 1 round."),
-    ...Array(1).fill("😵 Diarrhea: You need to rush immediately to the 🚾! All guests currently in the 🚾 leave the party."),
-    ...Array(1).fill("💡 Power Outage: (Roll 🎲) guests leave the party."),
-    ...Array(1).fill("🚑 Overdose: 2 guests leave with the ambulance."),
-    ...Array(1).fill("🥊 Bar Fight: The noise triggers a neighbor complaint."),
-    ...Array(1).fill("🔌 Unplugged System: Everyone leaves the dancefloor."),
-    ...Array(1).fill("🌿 The Munchies: (Roll 🎲) guests head to the kitchen to eat."),
-    ...Array(4).fill("🕺🏽 Night Fever: (Roll 🎲) guests head to the dancefloor."),
-    ...Array(1).fill("🍹 Shots!: (Roll 🎲) guests head to the bar."),
-    ...Array(1).fill("🚽 Pee Rush: (Roll 🎲) guests rush to the 🚾."),
-    ...Array(1).fill("🤮 Hold My Hair: 2 guests rush to the 🚾."),
-    ...Array(1).fill("🪠 EWWW: If you don't clean the 🚾, all guests needing the 🚾 will leave."),
-    ...Array(1).fill("🧻 No TP: You forgot to put a new roll, one nasty ass guests leaves the party."),
-    ...Array(3).fill("🔇 Neighbor Complaint 🔇 - If you collect 3 neighbor complaints, the police shows up and 6 guests must leave the party."),
-    ...Array(3).fill("🏺 Something broke: Find the guest responsible and kick it out of the party."),
-    ...Array(1).fill("😈 Unwanted Graffiti: 3 guests are vandalizing the walls, kick them out!."),
-    ...Array(1).fill("🚩 Red Flag: There are 2 idiots bullying random people, not cool, kick them out!."),
+    ...Array(6).fill("🎶 Change Music: <br> <br> Go to the dancefloor room and play any music card from your hand. <br> <br>If the music doesn't change now, 2 guests will leave the party."),
+    ...Array(3).fill("🔇 Neighbor Complaint 🔇 <br><br> If you collect 3 neighbor complaints, the police shows up and 6 guests must leave the party."),
+
+    ...Array(1).fill("🐈‍⬛ Unlucky: <br><br>Draw 2 more FCKUP cards."),
+
+    //...Array(1).fill("💤 Lazy Bastard: Discard 3 action cards from your hand. You can only play with 3 cards for the next round."),
+    //...Array(1).fill("🥴 Too Much to Drink: You can only play 1 action per round for the next 1 round."),
+
+    ...Array(1).fill("🥊 Bar Fight: <br><br>The noise triggers a neighbor complaint."),
+
+    ...Array(1).fill("😵 Diarrhea: <br><br>You need to rush immediately to the 🚾! All guests currently in the 🚾 leave the party."),
+
+    ...Array(1).fill("🤮 Hold My Hair: <br><br>2 guests rush to the 🚾."),
+    ...Array(1).fill("🪠 EWWW: If you don't clean the 🚾, all guests needing the 🚾 will leave until you clean it."),
+
+    ...Array(1).fill("💡 Power Outage: <br><br>(Roll 🎲) guests leave the party."),
+
+    ...Array(2).fill("🌿 The Munchies: <br><br>(Roll 🎲) guests head to the kitchen to eat."),
+    ...Array(2).fill("🕺🏽 Night Fever: <br><br>(Roll 🎲) guests head to the dancefloor."),
+    ...Array(2).fill("🍹 Shots!: <br><br>(Roll 🎲) guests head to the bar."),
+    ...Array(2).fill("🚽 Pee Rush: <br><br>(Roll 🎲) guests rush to the 🚾."),
+
+    ...Array(1).fill("🔌 Unplugged System: <br><br>Everyone leaves the dancefloor."),
+    //...Array(1).fill("🧻 No TP: <br><br>You forgot to put a new roll, one nasty ass guests leaves the party."),
+
+    ...Array(1).fill("🏺 Something broke: <br><br>Find the guest responsible and kick it out of the party."),
+    ...Array(1).fill("😈 Unwanted Graffiti:<br><br> 2 guests are vandalizing the walls, kick them out!."),
+    ...Array(1).fill("🚩 Red Flag:<br><br> There are 2 idiots bullying random people, not cool, kick them out!."),
+    ...Array(1).fill("🚑 Overdose:<br><br> 1 guest leaves with the ambulance."),
+
 
 ];
 
 
 const minimissionsDeck = [
-    ...Array(1).fill("🧚 Fairy Dusk: Visit the 🚾 and bring 2 guests with you (3 coins)"), 
-    ...Array(1).fill("🛏️ Hooked: Be alone in a bedroom with another guest (3 coins)"), 
-    ...Array(1).fill("🧑‍🍳 House Chef: Chill out in a full kitchen (5 coins)"), 
-    ...Array(1).fill("🧳 Nomad: Visit 3 different rooms in a single turn (2 coins)"), 
-    ...Array(1).fill("💃 Sweaty Dancefloor: Dance with 7 other guests on the dancefloor (2 coins)"), 
-    ...Array(1).fill("🧑‍🤝‍🧑 Hook-Up: Meet someone alone in the corridor (3 coins)"), 
-    ...Array(1).fill("🦠 Germophobe: Wash your hands, leave the 🚾, go back and wash again (2 coins)"), 
-    ...Array(1).fill("💊 Get Enhancers: Catch a disco queen in the corner that has what you are looking(2 coins)"), 
-    ...Array(1).fill("📚 Sophisto Prick: Suddenly feel like reading a book from the library (1 coin)"), 
-    ...Array(1).fill("😴 Powernap: Take a quick break in the bedroom (1 coin)"), 
-    ...Array(1).fill("🕵️ Creeper: Watch people dancing from a non-danceable corner (1 coin)"), 
-    ...Array(1).fill("🍾 Barman: Serve drinks at a full drinking station (5 coins)"),
+    ...Array(1).fill("🧚 Fairy Dusk: <br><br>Visit the 🚾 and bring 2 guests with you (5 coins)"), 
+    ...Array(1).fill("🛏️ Hooked: <br><br>Be alone in a bedroom with another guest (10 coins)"), 
+    ...Array(1).fill("🧳 Nomad: <br><br>Visit 3 different rooms in a single turn (5 coins)"), 
+    ...Array(1).fill("🧑‍🤝‍🧑 Hook-Up: <br><br>Meet someone alone in the corridor (5 coins)"), 
+    ...Array(1).fill("🦠 Germophobe: <br><br>Wash your hands, leave the 🚾, go back and wash again (5 coins)"), 
+    ...Array(1).fill("💊 Get Enhancers: <br><br>Catch a disco queen in the corner of the room (5 coins)"), 
+
+    ...Array(1).fill("📚 Sophisto Prick: <br><br>Suddenly feel like reading a book from the library (1 coin)"), 
+    ...Array(1).fill("😴 Powernap: <br><br>Take a quick break in the bedroom (1 coin)"), 
+    ...Array(1).fill("🕵️ Creeper: <br><br>Watch people dancing from a non-danceable corner (1 coin)"), 
+
+    ...Array(1).fill("🍾 Barman: <br><br>Serve drinks at a full drinking station (10 coins)"),
+    ...Array(1).fill("🧑‍🍳 House Chef: <br><br>Chill out in a full kitchen (10 coins)"), 
+    ...Array(1).fill("💃 Sweaty Dancefloor: <br><br>Dance with 7 other guests on the dancefloor (10 coins)"), 
+
 ];
 
 const PartyGoalsDeck = [
@@ -65,16 +81,19 @@ const PartyGoalsDeck = [
     ...Array(1).fill("Disco Fever: Most guests should be Disco fans and most songs played should be Disco (20 coins)"), 
     ...Array(1).fill("Karaoke Vibes: Most guests should be Pop fans and most songs played should be Pop (20 coins)"), 
     ...Array(1).fill("Mosh Pit: Most guests should be Rock fans and most songs played should be Rock (20 coins)"),
-    ...Array(1).fill("Rap Battle: Most guests should be Hip-Hop fans and most songs played should be Hip-Hop (20 coins)"),  
-    ...Array(1).fill("A Proper Mixer: Have 15 guests of each gernre at the end of the party (15 coins)"),  
+    ...Array(1).fill("Rap Battle: Most guests should be Hip-Hop fans and most songs played should be Hip-Hop (20 coins)"), 
+
+    ...Array(1).fill("A Proper Mixer: Have 8 or more guests of each gernre at the end of the party (20 coins)"),  
+
     ...Array(1).fill("Hood Party Ese: Have a mayority Hip-Hop and Latin music fans at the end of the party (15 coins)"),
     ...Array(1).fill("Electro Clash: Have a mayority Techno and Rock music fans at the end of the party (15 coins)"),
     ...Array(1).fill("Disco Divas Night: Have a mayority Disco and Pop music fans at the end of the party (15 coins)"),
     ...Array(1).fill("Rage Against the Public Enemy: Have a mayority Rock and Hip-Hop music fans at the end of the party (15 coins)"),
     ...Array(1).fill("K-Pop night: Have a mayority Pop and Techno music fans at the end of the party (15 coins)"),
+
     ...Array(1).fill("Beer Fest: Have 3 or more drink stations running at the end of the party (10 coins)"),
     ...Array(1).fill("Disco Guards: Have a disco queen in every room at the end of the party (10 coins)"),   
-    ...Array(1).fill("TikTok Dance Crew: Have a group of 6 poppers dancing at the end of the party (10 coins)"),  
+    ...Array(1).fill("TikTok Dance Crew: Have a group of 6 poppers dancing together at the end of the party (10 coins)"),  
 ];
 
 /*const htmlOutput = playerDeck.join('<br><br>').replace(/\n/g, '<br>');
@@ -154,7 +173,7 @@ document.getElementById("grab-action-cards-btn").addEventListener("click", () =>
 document.getElementById("round-card-btn").addEventListener("click", () => {
     const shuffledDeck = shuffle(fuckupsDeck);
     const card = shuffledDeck[0];
-    roundCardDiv.textContent = `FCKUP/ ${card}`;
+    roundCardDiv.innerHTML = `<h3>FCKUP</h3><br><br> ${card}<br><br>`;
     logList.innerHTML += `<li>FCKUP/ ${card}</li>`;
 });
 
@@ -179,7 +198,7 @@ document.getElementById("mini-mission-btn").addEventListener("click", () => {
         newCardDiv.className = "round-card";
 
         newCardDiv.innerHTML = `
-            <span>MINI MISSION/ ${card}</span>
+            <span><h3>MINI MISSION</h3><br><br> ${card}<br><br></span>
             <button class="resolve-btn">✔ Resolved</button>
         `;
 
@@ -236,7 +255,7 @@ document.getElementById("party-goals-btn").addEventListener("click", () => {
 
             // Set the inner HTML of the card
             cardDiv.innerHTML = `
-                <span>PARTY GOAL/ ${card}</span>
+                <span><h3>PARTY GOAL</h3> <br> <br> ${card} <br> <br></span>
                 <div class="card-buttons">
                     <button class="resolve-btn">✔ Resolved</button>
                     <button class="discard-btn">✖ Discard</button>
